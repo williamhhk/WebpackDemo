8b5d33c309f9c677eca484b72eaf5718a8f3cde5
william.han@gmail.com <william.han@gmail.com>
2017-01-08 13:32:45 -0700

    Add project files.

---
+++ WebpackDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebpackDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebpackDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("21c5ce69-0e20-42e9-a4ba-d78d1a15b015")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebpackDemo/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './index',
+  output: {
+    filename: 'browser-bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+        {
+            test: /\.jsx?$/,         // Match both .js and .jsx files
+            exclude: /node_modules/,
+            loader: "babel-loader",
+            query: {
+                presets: ['es2015', 'react']
+            }
+        },
+
+    ]
+  }
+};
